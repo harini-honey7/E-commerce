bb4ca3eaf6cb947b486adebf80fdfffd9aa34c45
harini <saiharinikona@gmail.com>
2025-09-01 12:41:18 +0530

    Fix: correct Stripe import for ESM

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,6 @@
 import orderModel from '../models/orderModel.js';
 import userModel from '../models/userModel.js';
-import Stripe from 'Stripe';
+import Stripe from 'stripe';
 import razorpay from 'razorpay';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
